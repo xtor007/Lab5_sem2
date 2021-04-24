75fef066ff95084f760e4a213fdb3831129225c1
Anatoliy <tolxpams@gmail.com>
2021-04-24 12:06:17 +0300

    add class point

---
+++ projectFiles/Point.hpp
@@ -0,0 +1,29 @@
+//
+//  Point.hpp
+//  lab5
+//
+//  Created by Anatoliy Khramchenko on 24.04.2021.
+//
+
+#ifndef Point_hpp
+#define Point_hpp
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class Point {
+public:
+    float latitude;
+    float longitude;
+    float x;
+    float y;
+    string type;
+    string subtype;
+    string name;
+    string adress;
+    
+    Point (float latitude, float longitude, string type, string subtype, string name, string adress);
+};
+
+#endif /* Point_hpp */
