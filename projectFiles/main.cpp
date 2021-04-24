3f9e9f68c02b611b6f90c513075eeef632f9fcb6
Anatoliy <tolxpams@gmail.com>
2021-04-24 13:31:35 +0300

    delete code for test

--- projectFiles/main.cpp
+++ projectFiles/main.cpp
@@ -10,20 +10,5 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    Point p1; p1.x=3; p1.y=4;
-    Point p2; p2.x=5; p2.y=-5;
-    Point p3; p3.x=6; p3.y=0;
-    Point p4; p4.x=7; p4.y=4;
-    Point p5; p5.x=3; p5.y=-4;
-    Point p6; p6.x=2; p6.y=7;
-    
-    RTree tree;
-    tree.addPoint(&p1);
-    tree.addPoint(&p2);
-    tree.addPoint(&p3);
-    tree.addPoint(&p4);
-    tree.addPoint(&p5);
-    tree.addPoint(&p6);
-    
     return 0;
 }
