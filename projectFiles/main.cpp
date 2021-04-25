9dcd76aab7622ecdc822e3b61aa91a65a8b085ab
Anatoliy <tolxpams@gmail.com>
2021-04-25 10:44:42 +0300

    delete code for test

--- projectFiles/main.cpp
+++ projectFiles/main.cpp
@@ -13,23 +13,5 @@ using namespace std;
 
 
 int main(int argc, const char * argv[]) {
-    Point p1; p1.x=3; p1.y=4;
-    Point p2; p2.x=-3; p2.y=5;
-    Point p3; p3.x=3; p3.y=-4;
-    Point p4; p4.x=6; p4.y=5;
-    Point p5; p5.x=7; p5.y=8;
-    Point p6; p6.x=1; p6.y=2;
-    
-    RTree tree;
-    tree.addPoint(&p1);
-    tree.addPoint(&p2);
-    tree.addPoint(&p3);
-    tree.addPoint(&p4);
-    tree.addPoint(&p5);
-    tree.addPoint(&p6);
-    
-    Point fp; fp.x=2; fp.y=2;
-    vector<Point*> res = tree.findNear(fp, 4);
-    
     return 0;
 }
